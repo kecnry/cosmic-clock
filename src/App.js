a508bce209e6bb182be4ac922ff68657536c7edd
Kyle Conroy <kyleconroy@gmail.com>
2018-09-02 15:12:01 -0400

    added separate close button for settings
    and improved responsive spacing of settings buttons

--- src/App.js
+++ src/App.js
@@ -59,8 +59,10 @@ export default class App extends Component {
         <Settings showSettings={this.state.showSettings} date={this.state.date} onChange={this.onChange} />
         <ColorSettings showSettings={this.state.showColorSettings} bgColor={this.state.bgColor} fgColor={this.state.fgColor} onChange={this.onChange} />
 
-        <ToggleButton onClick={this.toggleSettings} style={{position: "absolute", bottom: "25px", right: "25px"}} iconColor={this.state.fgColor} iconClass={'fas fa-2x fa-cog'}/>
-        <ToggleButton onClick={this.toggleColorSettings} style={{position: "absolute", bottom: "25px", right: "75px"}} iconColor={this.state.fgColor} iconClass={'fas fa-2x fa-palette'}/>
+        <div style={{position: "absolute", bottom: "2%", right: "2%"}}>
+          <ToggleButton onClick={this.toggleColorSettings} style={{paddingLeft: "10px"}} iconColor={this.state.fgColor} iconClass={'fas fa-2x fa-palette'}/>
+          <ToggleButton onClick={this.toggleSettings} style={{paddingLeft: "10px"}} iconColor={this.state.fgColor} iconClass={'fas fa-2x fa-cog'}/>
+        </div>
 
         <Clock size={size} bgColor={this.state.bgColor} fgColor={this.state.fgColor} date={this.state.date} location={this.state.location}/>
 
@@ -78,6 +80,7 @@ class ToggleButton extends Component {
 }
 
 class Settings extends Component {
+  onCloseSettings = () => this.props.onChange({showSettings: false});
   onChangeDateTime = date => this.props.onChange({date: date});
   onChangeLocation = location => this.props.onChange({location: location});
 
@@ -87,12 +90,16 @@ class Settings extends Component {
       display = 'none'
     }
     return (
-      <div className='Settings' style={{position: "fixed", width: "100%", height: "100%", paddingTop: "50px", backgroundColor: "rgba(224, 238, 255, 0.79)", display: display}}>
-        <div className='SettingsSection'>
-          Date and Time<br/><DateTimePicker onChange={this.onChangeDateTime} value={this.props.date}/>
-        </div>
-        <div className='SettingsSection'>
-          Location<br/><Geosuggest onSuggestSelect={this.onChangeLocation}/>
+      <div style={{position: "fixed", width: "100%", height: "100%", paddingTop: "50px", backgroundColor: "rgba(224, 238, 255, 0.79)", display: display}}>
+      <ToggleButton onClick={this.onCloseSettings} style={{position: "absolute", top: "25px", left: "25px"}} iconColor={'black'} iconClass={'fas fa-3x fa-times'}/>
+
+        <div className='Settings' style={{paddingTop: '5px'}}>
+          <div className='SettingsSection'>
+            Date and Time<br/><DateTimePicker onChange={this.onChangeDateTime} value={this.props.date}/>
+          </div>
+          <div className='SettingsSection'>
+            Location<br/><Geosuggest onSuggestSelect={this.onChangeLocation}/>
+          </div>
         </div>
       </div>
     )
@@ -100,6 +107,7 @@ class Settings extends Component {
 }
 
 class ColorSettings extends Component {
+  onCloseSettings = () => this.props.onChange({showColorSettings: false});
   onChangebgColor = color => this.props.onChange({bgColor: "rgba("+color.rgb.r+","+color.rgb.g+","+color.rgb.b+","+color.rgb.a+")"});
   onChangefgColor = color => this.props.onChange({fgColor: "rgba("+color.rgb.r+","+color.rgb.g+","+color.rgb.b+","+color.rgb.a+")"});
   render() {
@@ -108,12 +116,16 @@ class ColorSettings extends Component {
       display = 'none'
     }
     return (
-      <div className='Settings' style={{position: "fixed", width: "100%", height: "100%", paddingTop: "50px", backgroundColor: "rgba(224, 238, 255, 0.79)", display: display}}>
-        <div className='SettingsSection'>
-          Background Color<br/><div style={{margin: 'auto', width: '225px'}}><ChromePicker onChangeComplete={this.onChangebgColor} color={this.props.bgColor}/></div>
-        </div>
-        <div className='SettingsSection'>
-          Foreground Color<br/><div style={{margin: 'auto', width: '225px'}}><ChromePicker onChangeComplete={this.onChangefgColor} color={this.props.fgColor}/></div>
+      <div style={{position: "fixed", width: "100%", height: "100%", paddingTop: "50px", backgroundColor: "rgba(224, 238, 255, 0.79)", display: display}}>
+      <ToggleButton onClick={this.onCloseSettings} style={{position: "absolute", top: "25px", left: "25px"}} iconColor={'black'} iconClass={'fas fa-3x fa-times'}/>
+
+        <div className='Settings' style={{paddingTop: '5px'}}>
+          <div className='SettingsSection'>
+            Background Color<br/><div style={{margin: 'auto', width: '225px'}}><ChromePicker onChangeComplete={this.onChangebgColor} color={this.props.bgColor}/></div>
+          </div>
+          <div className='SettingsSection'>
+            Foreground Color<br/><div style={{margin: 'auto', width: '225px'}}><ChromePicker onChangeComplete={this.onChangefgColor} color={this.props.fgColor}/></div>
+          </div>
         </div>
       </div>
     )
