18d6912f5a46c80bcf1f57f6ab67a246707d0172
Kyle Conroy <kyleconroy@gmail.com>
2018-09-18 17:08:15 -0400

    fixed typo in fractional hour for event end

--- src/clock.jsx
+++ src/clock.jsx
@@ -308,7 +308,7 @@ export default class Clock extends Component {
         if (eventDateEnd - this.state.date < 24*60*60*1000 && eventDateEnd - this.state.date > 0) {
 
           rDayEventStart = (eventDateStart.getHours() + eventDateStart.getMinutes()/60) / 24
-          rDayEventEnd = (eventDateEnd.getHours() + eventDateStart.getMinutes()/60) / 24
+          rDayEventEnd = (eventDateEnd.getHours() + eventDateEnd.getMinutes()/60) / 24
           eventTooltip = this.state.calendarEvents[i].summary + " ("+eventDateStart.toLocaleDateString()+" "+eventDateStart.toLocaleTimeString()+"-"+eventDateEnd.toLocaleTimeString()+")"
           eventLink = this.state.calendarEvents[i].htmlLink
           calendarDay.push(<CircleSegment cx={cx} cy={cy}
