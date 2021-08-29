26a60e998a226e8e211cc40183e64e91e94d1aeb
Kyle Conroy <kyleconroy@gmail.com>
2021-08-29 11:20:48 -0400

    Updates

---
+++ precache-manifest.f53b4e4e00666f822db09a4ab22c3b1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84e96eea9429e30bc4cf",
+    "url": "/cosmic-clock/static/js/runtime~main.222fbab7.js"
+  },
+  {
+    "revision": "51fafd4e9f5b684df6b9",
+    "url": "/cosmic-clock/static/js/main.023b08c8.chunk.js"
+  },
+  {
+    "revision": "250ca9bb67d70a918c74",
+    "url": "/cosmic-clock/static/js/2.265dcae1.chunk.js"
+  },
+  {
+    "revision": "51fafd4e9f5b684df6b9",
+    "url": "/cosmic-clock/static/css/main.ebc1e4f0.chunk.css"
+  },
+  {
+    "revision": "250ca9bb67d70a918c74",
+    "url": "/cosmic-clock/static/css/2.193d13cd.chunk.css"
+  },
+  {
+    "revision": "2abd56fbb01fe5eeebf708133c27568f",
+    "url": "/cosmic-clock/index.html"
+  }
+];
